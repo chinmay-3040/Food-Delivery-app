b6454ce03af5e2115351e747d0efc4ae170dda4c
Chinmay Dawalbaje <chinmaydawalbaje129@gmail.com>
2024-05-11 00:35:20 +0530

    added env variables5

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,3 +24,8 @@ const UserSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', UserSchema);
+
+// So, in summary, the line module.exports = mongoose.model('User', UserSchema); exports a Mongoose model 
+// named 'User' with the specified schema UserSchema, making it accessible for use in other parts of your Node.js 
+// application. Other files can import this model using require() and then interact with the MongoDB collection 
+// associated with the 'User' model.
